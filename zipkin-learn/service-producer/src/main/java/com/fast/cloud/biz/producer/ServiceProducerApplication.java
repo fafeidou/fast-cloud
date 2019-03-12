dffe30f95a8ff653b422a8c1a26f320759398df2
Batman.qin <943104990@qq.com>
2019-03-12 14:29:19 +0800

    zipkin client add

--- zipkin-learn/service-producer/src/main/java/com/fast/cloud/biz/producer/ServiceProducerApplication.java
+++ zipkin-learn/service-producer/src/main/java/com/fast/cloud/biz/producer/ServiceProducerApplication.java
@@ -1,14 +1,27 @@
 package com.fast.cloud.biz.producer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ServiceProducerApplication {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     public static void main(String[] args) {
         SpringApplication.run(ServiceProducerApplication.class, args);
+    }
+
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
+    String trace() {
+        logger.info("trace product");
+        return "trace";
     }
 
 }
