f70cea10696d45f707ad4bba4dad583d14db8677
Batman.qin <943104990@qq.com>
2019-03-15 16:11:22 +0800

    mybatis gen init

---
+++ fast-cloud-common/common-mybatis/mybatis-plus-gen/src/test/java/com/fast/cloud/mybatis/plus/gen/MybatisPlusGenApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fast.cloud.mybatis.plus.gen;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MybatisPlusGenApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
