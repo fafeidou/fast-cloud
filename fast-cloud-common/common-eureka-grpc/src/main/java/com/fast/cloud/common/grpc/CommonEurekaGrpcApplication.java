a78b16ff69eab503dbd4bf02483f336051007cac
qfx <qinfuxiang>
2020-01-08 11:11:03 +0800

    grpc eureka init

---
+++ fast-cloud-common/common-eureka-grpc/src/main/java/com/fast/cloud/common/grpc/CommonEurekaGrpcApplication.java
@@ -0,0 +1,13 @@
+package com.fast.cloud.common.grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonEurekaGrpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonEurekaGrpcApplication.class, args);
+    }
+
+}
---
+++ fast-cloud-eureka-grpc/src/main/java/com/fast/cloud/eureka/grpc/FastCloudEurekaGrpcApplication.java
@@ -0,0 +1,13 @@
+package com.fast.cloud.eureka.grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastCloudEurekaGrpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastCloudEurekaGrpcApplication.class, args);
+    }
+
+}
