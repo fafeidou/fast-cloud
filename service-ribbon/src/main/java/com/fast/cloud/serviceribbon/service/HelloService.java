14cf551f5a6a94975f387ec8538b350cef4f28a9
qinfuxiang <17521222912@163.com>
2020-04-06 22:32:06 +0800

    ribbon add hystrix

--- service-ribbon/src/main/java/com/fast/cloud/serviceribbon/service/HelloService.java
+++ service-ribbon/src/main/java/com/fast/cloud/serviceribbon/service/HelloService.java
@@ -1,5 +1,6 @@
 package com.fast.cloud.serviceribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -16,8 +17,13 @@ public class HelloService {
     @Autowired
     RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name) {
         return restTemplate.getForObject("http://SERVICE-HI/hi?name=" + name, String.class);
+    }
+
+    public String hiError(String name) {
+        return "name: " + name + "sevice hi is error";
     }
 
 
